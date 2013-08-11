a76f701ccf8a991f2c287aebfff9cfe022370c87
nickpeachey <nick.peachey@gmail.com>
2013-08-11 11:34:19 +0100

    setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memfy::Application.config.secret_token = '8a7d3d620e6522ab93bf3bce295f58b8db5bcd40323590c2d44483e97619bafb0108391404f5e78445aa1ce1dc9c3b906b733168bd2cecea1942813e64c5dcad'
