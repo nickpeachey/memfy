9421fa071728765aaab0bd935744e766e3f6c60d
nickpeachey <nick.peachey@gmail.com>
2013-08-11 12:28:37 +0100

    added bootstrap layout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,6 @@ Memfy::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  config.assets.precompile = false
-
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
